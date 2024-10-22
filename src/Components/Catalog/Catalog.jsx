0c41a72357d6ef095c9655c6f846c9acc1cf97bf
muhammadamin-web <mirboqijonov@gmail.com>
2024-10-22 14:26:25 +0500

    push

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -1,56 +1,81 @@
-import React from 'react'
-import man from '../../assets/images/man.svg'
-import wal from '../../assets/images/wal.svg'
-import catalog from '../../assets/images/catalog.png'
+import React from "react";
+import man from "../../assets/images/man.svg";
+import wal from "../../assets/images/wal.svg";
+import catalog from "../../assets/images/catalog.png";
+import pin_adress from "../../assets/images/pin_adress.svg";
 
 const Catalog = () => {
-    return (
-        <div className='catalog'>
-            <div className="container">
-                <div className="catalog_box">
-                    <div className="catalog_card">
-                        <div className="catalog_top">
-                            <div className="catalog_top_card">
-                                <div className="catalog_top_card_element">
-                                    <div className="catalog_border1"></div>
-                                    <p className="catalog_top_text">
-                                        Черкизовская
-                                    </p>
-                                </div>
-                                <div className="catalog_top_card_element">
-                                    <div className="catalog_border2"></div>
-                                    <p className="catalog_top_text">
-                                        Щелковская
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="catalog_top_card_element">
-                                <img src={man} alt="" />
-                                <p className="catalog_top_text">25 мин</p>
-                            </div>
-                            <div className="catalog_top_card_element">
-                                <img src={wal} alt="" />
-                                <p className="catalog_top_text">Умные планировки</p>
-                            </div>
-                        </div>
-                        <div className="catalog_card_img">
-                            <img className='catalog_img' src={catalog} alt="" />
-                            <div className="catalog_bottom">
-                                <div className="catalog_bottom_card">
-                                    <p className="catalog_subtitle">2-й Иртышский</p>
-                                    <p className="catalog_subtitle2">От 6 914 540 ₽</p>
-                                </div>
-                                <div className="catalog_bottom_card">
-                                    <p className="catalog_bottom_text">(dan) 47.6 ga 78,28 м²</p>
-                                    <p className="catalog_bottom_text">300 kvartira sotiladi</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="catalog">
+      <div className="container">
+        <div className="catalog_box">
+          <div className="catalog_card">
+            <div className="catalog_card_img">
+              <img className="catalog_img" src={catalog} alt="" />
+              <div className="catalog_bottom">
+                <div className="catalog_bottom_card">
+                  <p className="catalog_subtitle">Orifon</p>
+                  <p className="catalog_subtitle2">298.9 mln UZS dan</p>
+                  <p className="catalog_bottom_text">300 kvartira sotiladi</p>
                 </div>
+                <div className="catalog_bottom_card">
+                  <p class="card_palace">
+                    <img className="pin_address" src={pin_adress} alt="" />
+                    Tashkent viloyati
+                  </p>
+                  <p className="catalog_bottom_text">
+                    39,8м² (dan) - 111м² (gacha)
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
+          <div className="catalog_card">
+            <div className="catalog_card_img">
+              <img className="catalog_img" src={catalog} alt="" />
+              <div className="catalog_bottom">
+                <div className="catalog_bottom_card">
+                  <p className="catalog_subtitle">Orifon</p>
+                  <p className="catalog_subtitle2">298.9 mln UZS dan</p>
+                  <p className="catalog_bottom_text">300 kvartira sotiladi</p>
+                </div>
+                <div className="catalog_bottom_card">
+                  <p class="card_palace">
+                    <img className="pin_address" src={pin_adress} alt="" />
+                    Tashkent viloyati
+                  </p>
+                  <p className="catalog_bottom_text">
+                    39,8м² (dan) - 111м² (gacha)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="catalog_card">
+            <div className="catalog_card_img">
+              <img className="catalog_img" src={catalog} alt="" />
+              <div className="catalog_bottom">
+                <div className="catalog_bottom_card">
+                  <p className="catalog_subtitle">Orifon</p>
+                  <p className="catalog_subtitle2">298.9 mln UZS dan</p>
+                  <p className="catalog_bottom_text">300 kvartira sotiladi</p>
+                </div>
+                <div className="catalog_bottom_card">
+                  <p class="card_palace">
+                    <img className="pin_address" src={pin_adress} alt="" />
+                    Tashkent viloyati
+                  </p>
+                  <p className="catalog_bottom_text">
+                    39,8м² (dan) - 111м² (gacha)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Catalog
+export default Catalog;
